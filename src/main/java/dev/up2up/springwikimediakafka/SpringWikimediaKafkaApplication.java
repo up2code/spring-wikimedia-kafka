174a7b3432fb545437be2344948e76195cac0177
Augsorn Chanklad <uppban5@gmail.com>
2024-12-17 23:48:06 +0700

    Initial Commit

---
+++ src/main/java/dev/up2up/springwikimediakafka/SpringWikimediaKafkaApplication.java
@@ -0,0 +1,13 @@
+package dev.up2up.springwikimediakafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWikimediaKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWikimediaKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/up2up/springwikimediakafka/SpringWikimediaKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.up2up.springwikimediakafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWikimediaKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
